d68cccdba26d0582d799e2473dc1b065ead5d1ef
devkavin <kavindra.senanayake@gmail.com>
2024-08-13 11:12:54 +0530

    Add CourseScreen, Home, and Profile screens

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lms_mob_app/services/user_services.dart';
+
+import 'course_screen.dart';
+import 'login.dart';
+import 'profile.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,15 +13,66 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int currentIndex = 0;
+
+  final List<Widget> _screens = [
+    CourseScreen(),
+    Profile(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              logout().then((value) => {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) => Login()),
+                        (route) => false),
+                  });
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Welcome to the home screen'),
+      body: _screens[currentIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.of(context).pushNamed('/courses');
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        elevation: 10,
+        clipBehavior: Clip.antiAlias, // styling
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 0;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {
+                setState(() {
+                  currentIndex = 1;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
